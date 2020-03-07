e986c303252cfbf91f620e9b11c400116dc68c51
Jan Podmolík <wexly.ue@gmail.com>
2020-03-07 15:06:13 +0100

    Initial Commit

---
+++ MUSCLE UAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MUSCLE UAPP
+//
+//  Created by Johana Šlechtová on 07/03/2020.
+//  Copyright © 2020 Jan Podmolík. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
