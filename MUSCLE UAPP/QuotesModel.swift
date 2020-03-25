741467513550fbe05e69a93369f44d98612d7713
Jan Podmolík <wexly.ue@gmail.com>
2020-03-25 21:10:43 +0100

    first commit

---
+++ MUSCLE UAPP/QuotesModel.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesModel.swift
+//  Muscle UApp
+//
+//  Created by Johana Šlechtová on 04/02/2020.
+//  Copyright © 2020 Jan Podmolík. All rights reserved.
+//
+
+import SwiftUI
+
+//MARK: - QuotesModel
+
+struct Quotes: Identifiable {
+    var id = UUID()
+    var content: String
+}
+
